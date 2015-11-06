9fc73c99c1ac41c03eaece79bf4e15f430bb4303
Mexahoid <mirramax97@Mail.ru>
2015-11-06 18:05:47 +0300

    Snake

---
+++ Проги/Snake/Snake/Point.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Snake
+{
+  class Point
+  {
+    public int x;
+    public int y;
+    public char sm;
+
+    public Point()
+    {      
+    }
+
+    public Point(int x, int y, char sm)
+    {
+      this.x = x;
+      this.y = y;
+      this.sm = sm;
+    }
+
+    public void SetSymbol()
+    {
+      Console.SetCursorPosition(x, y);
+      Console.Write(sm);
+    }
+  }
+}
---
+++ Проги/Snake/Snake/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Snake
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Point p1 = new Point(3, 7, '*');
+      p1.SetSymbol();
+      Point p2 = new Point(7, 6, '&');
+      p2.SetSymbol();
+      Console.ReadLine();
+    }
+  }
+}
